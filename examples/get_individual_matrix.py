dd7f4cfb203d48a571f8e4d02d791fb178620113
Bernard Roesler <bernard.roesler@gmail.com>
2025-06-10 15:01:17 -0400

    Notes.

--- examples/get_individual_matrix.py
+++ examples/get_individual_matrix.py
@@ -31,10 +31,12 @@ if __name__ == "__main__":
     # mat_id = 382   # DRIVCAV/cavity03 has 'x'
     # mat_id = 2137  # JGD_Kocay/Trec4 has 'notes'
     # mat_id = 449   # Grund/b1_ss  has 'b'
-    mat_id = 2396  # Newman/dolphins has 'aux' (single item)
+    # mat_id = 2396  # Newman/dolphins has 'aux' (single item)
     # mat_id = 1487  # Pajek/GD95_c has 'aux' (list of ['nodename', 'coords'])
     # mat_id = 1457  # Pajek/Cities has 'aux' data (list of ['cluster', 'colname','rowname'])
     # NOTE all Pajek matrices seem to be graphs with 'aux' data.
+    # mat_id = 1440  # Oberwolfach/LFATS 'notes' is one line
+    mat_id = 2734  # VDOL/spaceStation_1 'aux' 'rowname' is long list of strings
 
     problem = get_ss_problem(
         index=df,
